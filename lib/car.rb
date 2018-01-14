f99818b15cd4e79b4f069c42a0cccf527cfc19fa
bennybop <bennybop74@yahoo.com>
2018-01-14 22:37:32 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,8 +1,19 @@
 require_relative "./vehicle.rb"
-class Car
 
-  def initialize(name,car)
-    @name = name
-    @car = car
+class Vehicle
+
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
   end
-end 
+
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
+end
